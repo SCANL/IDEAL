8346080c315dd797f6de5e7fd1b2755970c5f5d4
Adam Styer <als1543@rit.edu>
2021-09-11 20:57:26 -0400

    Added sample model files

---
+++ src/main/java/com/github/astyer/naturallanguagelabplugin/model/Identifier.java
@@ -0,0 +1,11 @@
+package com.github.astyer.naturallanguagelabplugin.model;
+
+import com.intellij.psi.PsiElement;
+
+public interface Identifier {
+    public String getName();
+    public int getLineNumber();
+    public int getColumnNumber();
+    public String getType();
+    public PsiElement getPsiObject();
+}
