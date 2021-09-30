665e5f9b6a709d8acdea7a3a155d7c1e0a449cb9
Adam Styer <als1543@rit.edu>
2021-09-29 22:10:21 -0400

    Connecting the pipes

---
+++ src/main/java/com/github/astyer/naturallanguagelabplugin/IR/IRFactory.java
@@ -0,0 +1,17 @@
+package com.github.astyer.naturallanguagelabplugin.IR;
+
+import com.intellij.psi.PsiArrayType;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.PsiVariable;
+
+public class IRFactory {
+
+    public Variable createVariable(PsiVariable psiVariable) {
+        PsiType type = psiVariable.getType();
+        String typeString = type.toString();
+        if (type instanceof PsiArrayType) {
+            typeString = "Array";
+        }
+        return new Variable(psiVariable.getName(), 0, 0, typeString, psiVariable);
+    }
+}
