30c1d58408f638f5c517acf24e25fba523a80055
niravbarman <nxb7819@rit.edu>
2021-10-01 14:19:15 -0400

    Added in Method IR and updated RuleVisitor

---
+++ src/main/java/com/github/astyer/naturallanguagelabplugin/IR/Method.java
@@ -0,0 +1,71 @@
+package com.github.astyer.naturallanguagelabplugin.IR;
+
+import com.github.astyer.naturallanguagelabplugin.rules.POSTagger;
+import com.github.astyer.naturallanguagelabplugin.rules.Result;
+import com.github.astyer.naturallanguagelabplugin.rules.RuleVisitor;
+import com.intellij.psi.PsiElement;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Method implements Identifier{
+    String name, type, pos;
+    int lineNumber, colNumber;
+    PsiElement element;
+
+    public Method(String name, int lineNumber, int colNumber, String type, PsiElement element){
+        this.name = name;
+        this.lineNumber = lineNumber;
+        this.colNumber = colNumber;
+        this.type = type;
+        this.element = element;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getLineNumber() {
+        return this.lineNumber;
+    }
+
+    @Override
+    public int getColumnNumber() {
+        return this.colNumber;
+    }
+
+    @Override
+    public String getType() {
+        return this.type;
+    }
+
+    @Override
+    public PsiElement getPsiObject() {
+        return this.element;
+    }
+
+    @Override
+    public Optional<Result> accept(RuleVisitor visitor) {
+        return visitor.visitMethod(this);
+    }
+
+    @Override
+    public String getPOS() {
+        if(pos == null){
+            pos = POSTagger.getInstance().tag(getName());
+        }
+        return pos;
+    }
+
+    @Override
+    public Identifier getParent() {
+        return null;
+    }
+
+    @Override
+    public List<Identifier> getChildren() {
+        return null;
+    }
+}
