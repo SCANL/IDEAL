d67772e7987289b8ca139b213d6a5c498c8c9859
Styer, Adam <als1543@rit.edu>
2021-11-21 17:55:24 -0500

    Event driven detection and casting cleanup (#86)
    * fixed null pointer error
    
    * added event driven detection and cleand up casting detection
    
    Co-authored-by: als1543 <als1543@g.rit.edu>

--- src/main/java/com/github/astyer/naturallanguagelabplugin/IR/IRFactory.java
+++ src/main/java/com/github/astyer/naturallanguagelabplugin/IR/IRFactory.java
@@ -2,22 +2,24 @@ package com.github.astyer.naturallanguagelabplugin.IR;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiTypesUtil;
 
 import java.util.*;
 
 public class IRFactory {
 
-    private static HashMap<PsiType, Boolean> isCollectionPsiTypeCache = new HashMap<>();
-
     // maybe move these helper functions into a helper class
+    /**
+     * Checks if the PsiType is some kind of collection
+     * Currently checks for standard arrays and anything that is a Collection or Map
+     * @param type
+     * @param project
+     * @return if the PsiType is some kind of collection
+     */
     private static boolean isSomeCollection(PsiType type, Project project) {
         if(type instanceof PsiArrayType) {
             return true;
-        }
-
-        Boolean cachedIsCollectionResult = isCollectionPsiTypeCache.get(type);
-        if(cachedIsCollectionResult != null) {
-            return cachedIsCollectionResult;
         }
 
         PsiElementFactory psiElementFactory = PsiElementFactory.getInstance(project);
@@ -26,10 +28,8 @@ public class IRFactory {
 
         //check if the type is assignable to Collection or Map
         if(collectionPsiType.isAssignableFrom(type) || mapPsiType.isAssignableFrom(type)) {
-            isCollectionPsiTypeCache.put(type, true);
             return true;
         }
-        isCollectionPsiTypeCache.put(type, false);
         return false;
     }
 
@@ -37,11 +37,50 @@ public class IRFactory {
         if(type == null) {
             return "null";
         }
-        if (isSomeCollection(type, project)) {
+        if(isSomeCollection(type, project)) {
             return "Array";
         } else {
             return type.getCanonicalText();
         }
+    }
+
+    /**
+     * Check if psiMethod performs any kind of type conversions
+     * Currently only checks for any explicit casting
+     * @param psiMethod
+     * @return if psiMethod performs any kind of type conversions
+     */
+    private static boolean performsConversion(PsiMethod psiMethod) {
+        PsiTypeCastExpression castExpression = PsiTreeUtil.findChildOfType(psiMethod, PsiTypeCastExpression.class);
+        if(castExpression != null) {
+            System.out.println("cast type: " + castExpression.getType());
+            System.out.println("operand expression: " + castExpression.getOperand().getText());
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check if psiMethod overrides any methods from a class that is an event listener
+     * @param psiMethod
+     * @return if psiMethod overrides an event listener method
+     */
+    private static boolean performsEventDrivenFunctionality(PsiMethod psiMethod) {
+        PsiMethod[] superMethods = psiMethod.findSuperMethods();
+        if(superMethods.length > 0) {
+            PsiElementFactory psiElementFactory = PsiElementFactory.getInstance(psiMethod.getProject());
+            PsiType eventListenerPsiType = psiElementFactory.createTypeByFQClassName(EventListener.class.getName());
+            for(PsiMethod superMethod: superMethods) {
+                PsiClass parentClass = PsiTreeUtil.getParentOfType(superMethod, PsiClass.class);
+                if(parentClass != null) {
+                    PsiType parentClassType = PsiTypesUtil.getClassType(parentClass);
+                    if (eventListenerPsiType.isAssignableFrom(parentClassType)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public static Variable createVariable(PsiVariable psiVariable) {
@@ -51,14 +90,12 @@ public class IRFactory {
 
     public static Method createMethod(PsiMethod psiMethod) {
         String typeString = typeToString(psiMethod.getReturnType(), psiMethod.getProject());
-        boolean performsConversion = false; //traverse psi tree to figure out if any casting happens inside the method
-        boolean performsEventDrivenFunctionality = false;
 
-        List<String> params = new ArrayList();
+        List<String> params = new ArrayList(); //add params to the name because the POS tagger expects it
         for(PsiParameter param : psiMethod.getParameterList().getParameters()){
             params.add(typeToString(param.getType(), param.getProject()));
         }
         String name = psiMethod.getName() + "("+ String.join(",", params) + ")";
-        return new Method(name, typeString, performsConversion, performsEventDrivenFunctionality, psiMethod);
+        return new Method(name, typeString, performsConversion(psiMethod), performsEventDrivenFunctionality(psiMethod), psiMethod);
     }
 }
